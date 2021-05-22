c02d7fc538f3cc5600a2c57dbb68389259d857d8
EmirBirlik <ninjakoal2002@gmail.com>
2021-05-22 11:33:57 -0400

    edits

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import Button from './Button';
+//import PropTypes from 'prop-types';
+
 const Header = ({ title }) => {
   const handleClick = () => {
     console.log('Click');
   };
+
   return (
     <header className="header">
       <h1>{title}</h1>
@@ -15,4 +18,13 @@ const Header = ({ title }) => {
     </header>
   );
 };
+
+// Header.defaultProps = {
+//   title: 'Task Tracker',
+// };
+
+// Header.propTypes = {
+//   title: PropTypes.string.isRequired,
+// };
+
 export default Header;
