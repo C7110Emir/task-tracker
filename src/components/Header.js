e874e2704771a99a72fa823a1d7ba30cb0f2448d
EmirBirlik <ninjakoal2002@gmail.com>
2021-05-22 11:26:51 -0400

    edits

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Button from './Button';
+const Header = ({ title }) => {
+  const handleClick = () => {
+    console.log('Click');
+  };
+  return (
+    <header className="header">
+      <h1>{title}</h1>
+      <Button
+        color="purple"
+        text="Show Add Task Bar"
+        handleClick={handleClick}
+      />
+    </header>
+  );
+};
+export default Header;
